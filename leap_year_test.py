10cd8dec60a4ea6b8f4ab809ff7258206e113e4b
Punyawee <tonkla9874@gmail.com>
2022-01-26 14:06:18 +0700

    add leap year condition and leap year test

--- leap_year_test.py
+++ leap_year_test.py
@@ -1,11 +1,13 @@
-def check_year(year):
+def check_year(year):    
     if year % 400 == 0:
         return "Leap Year"
     elif year % 100 == 0:
         return "Century Year"
+    elif year % 4 == 0:
+        return "Leap Year"
     elif year % 100 != 0:
         return "Not century year"
-
+    
 
 def test_it_should_be_century_year_if_input_is_1900():
     assert check_year(1900) == "Century Year"
@@ -17,3 +19,6 @@ def test_it_should_be_leap_year_if_input_is_400():
 
 def test_it_should_not_be_century_year_if_input_is_1999():
     assert check_year(1999) == "Not century year"
+
+def test_it_should_be_leap_year_if_divided_by_4():
+    assert check_year(2004) == "Leap Year"
