0c15cdad8a869740f72930e4eecbac07ba835a51
theethaj <bank30827@gmail.com>
2022-01-26 14:00:46 +0700

    Pass year 1999 is not century year.

--- leap_year_test.py
+++ leap_year_test.py
@@ -3,6 +3,8 @@ def check_year(year):
         return "Leap Year"
     elif year % 100 == 0:
         return "Century Year"
+    elif year % 100 != 0:
+        return "Not century year"
 
 
 def test_it_should_be_century_year_if_input_is_1900():
@@ -12,3 +14,6 @@ def test_it_should_be_century_year_if_input_is_1900():
 def test_it_should_be_leap_year_if_input_is_400():
     assert check_year(400) == "Leap Year"
 
+
+def test_it_should_not_be_century_year_if_input_is_1999():
+    assert check_year(1999) == "Not century year"
